5349879bbc704bedc2c30382a3dde0a8659edc09
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-07-18 14:31:32 +0900

    add dependencies

--- Package.swift
+++ Package.swift
@@ -1,23 +1,47 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "ObfuscateMacro",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ObfuscateMacro",
-            targets: ["ObfuscateMacro"]),
+            targets: ["ObfuscateMacro"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git",
+                 from: "509.0.0-swift-DEVELOPMENT-SNAPSHOT-2023-06-05-a"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ObfuscateMacro"),
+            name: "ObfuscateMacro",
+            dependencies: [
+                "ObfuscateMacroPlugin"
+            ]
+        ),
+        .macro(
+            name: "ObfuscateMacroPlugin",
+            dependencies: [
+                .product(name: "SwiftSyntax", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
+                .product(name: "SwiftParserDiagnostics", package: "swift-syntax"),
+            ]
+        ),
         .testTarget(
             name: "ObfuscateMacroTests",
-            dependencies: ["ObfuscateMacro"]),
+            dependencies: ["ObfuscateMacro"]
+        ),
     ]
 )
---
+++ Sources/ObfuscateMacroPlugin/Macros/ObfuscatedString.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by p-x9 on 2023/07/18.
+//  
+//
+
+import Foundation
