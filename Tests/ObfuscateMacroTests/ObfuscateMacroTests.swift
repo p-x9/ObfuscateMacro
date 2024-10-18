f11709f1d2764b968ceaf2265ff947e25cdef040
Greg Cotten <greg@gregcotten.com>
2024-10-17 18:06:22 -0700

    add chachapoly oneshot

--- Tests/ObfuscateMacroTests/ObfuscateMacroTests.swift
+++ Tests/ObfuscateMacroTests/ObfuscateMacroTests.swift
@@ -40,6 +40,10 @@ final class ObfuscateMacroTests: XCTestCase {
         )
         XCTAssertEqual(
             "hello, こんにちは, 👪",
+            #ObfuscatedString("hello, こんにちは, 👪", method: .chaChaPoly)
+        )
+        XCTAssertEqual(
+            "hello, こんにちは, 👪",
             #ObfuscatedString("hello, こんにちは, 👪", repetitions: 5)
         )
     }
@@ -73,6 +77,13 @@ final class ObfuscateMacroTests: XCTestCase {
             #ObfuscatedString(
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
                 method: .AES
+            )
+        )
+        XCTAssertEqual(
+            original,
+            #ObfuscatedString(
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+                method: .chaChaPoly
             )
         )
     }
@@ -186,6 +197,37 @@ final class ObfuscateMacroTests: XCTestCase {
         )
     }
 
+    func testChaChaPoly() {
+        assertMacroExpansion(
+            """
+            let string = #ObfuscatedString(
+                "hello, こんにちは, 👪",
+                method: .chaChaPoly
+            )
+            """,
+            expandedSource: """
+            let string = {
+                var data = Data([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 153, 75, 135, 67, 211, 253, 144, 12, 70, 91, 197, 211, 13, 174, 59, 142, 242, 253, 96, 128, 122, 165, 138, 125, 34, 142, 193, 51, 49, 47, 130, 63, 235, 244, 3, 78, 29, 224, 83, 27, 251, 252, 78, 226])
+
+                data = try! ChaChaPoly.open(
+                    try! ChaChaPoly.SealedBox(
+                        combined: data
+                    ),
+                    using: SymmetricKey(
+                        data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                    )
+                )
+
+                return String(
+                    bytes: data,
+                    encoding: .utf8
+                )!
+            }()
+            """,
+            macros: macros
+        )
+    }
+
     func testRandomAll() {
         let originalSource = """
         let string = #ObfuscatedString(
@@ -219,12 +261,9 @@ final class ObfuscateMacroTests: XCTestCase {
             originalSource,
             expandedSource: """
             let string = {
-                var data = Data([150, 154, 108, 109, 109, 47, 36, 230, 135, 148, 235, 139, 153, 232, 141, 166, 237, 142, 177, 242, 147, 188, 56, 53, 230, 136, 137, 179])
+                var data = Data([97, 71, 86, 115, 98, 71, 56, 115, 73, 79, 79, 66, 107, 43, 79, 67, 107, 43, 79, 66, 113, 43, 79, 66, 111, 101, 79, 66, 114, 121, 119, 103, 56, 74, 43, 82, 113, 103, 61, 61])
 
-                data = Data(data.indexed().map { i, c in
-                    let i: UTF8.CodeUnit = UTF8.CodeUnit(i % Int(UInt8.max))
-                    return c ^ (254 &+ i)
-                })
+                data = Data(base64Encoded: String(data: data, encoding: .utf8)!)!
 
                 return String(
                     bytes: data,
@@ -240,9 +279,99 @@ final class ObfuscateMacroTests: XCTestCase {
             originalSource,
             expandedSource: """
             let string = {
-                var data = Data([97, 71, 86, 115, 98, 71, 56, 115, 73, 79, 79, 66, 107, 43, 79, 67, 107, 43, 79, 66, 113, 43, 79, 66, 111, 101, 79, 66, 114, 121, 119, 103, 56, 74, 43, 82, 113, 103, 61, 61])
+                var data = Data([253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 244, 45, 224, 228, 182, 8, 100, 223, 147, 25, 127, 140, 7, 99, 193, 162, 101, 238, 50, 30, 115, 188, 43, 134, 186, 218, 81, 148, 58, 208, 193, 243, 219, 54, 105, 106, 236, 244, 55, 104, 224, 78, 143, 42])
 
-                data = Data(base64Encoded: String(data: data, encoding: .utf8)!)!
+                data = try! AES.GCM.open(
+                    try! AES.GCM.SealedBox(
+                        combined: data
+                    ),
+                    using: SymmetricKey(
+                        data: [253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253]
+                    )
+                )
+
+                return String(
+                    bytes: data,
+                    encoding: .utf8
+                )!
+            }()
+            """,
+            macros: macros
+        )
+    }
+
+    func testRandomAllCrypto() {
+        let originalSource = """
+        let string = #ObfuscatedString(
+            "hello, こんにちは, 👪",
+            method: .randomAllCrypto
+        )
+        """
+
+        assertMacroExpansion(
+            originalSource,
+            expandedSource: """
+            let string = {
+                var data = Data([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 223, 66, 205, 116, 143, 77, 166, 74, 213, 199, 222, 24, 49, 109, 118, 142, 71, 240, 128, 241, 24, 96, 94, 98, 225, 144, 159, 161, 160, 205, 228, 140, 223, 237, 149, 198, 3, 111, 233, 223, 97, 98, 207, 140])
+
+                data = try! AES.GCM.open(
+                    try! AES.GCM.SealedBox(
+                        combined: data
+                    ),
+                    using: SymmetricKey(
+                        data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                    )
+                )
+
+                return String(
+                    bytes: data,
+                    encoding: .utf8
+                )!
+            }()
+            """,
+            macros: macros
+        )
+
+        ObfuscatedString.randomNumberGenerator = TestRandomNumberGenerator(seed: UInt64.max / 4 * 2)
+        assertMacroExpansion(
+            originalSource,
+            expandedSource: """
+            let string = {
+                var data = Data([254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 178, 215, 167, 56, 174, 219, 231, 10, 235, 83, 155, 41, 21, 84, 111, 41, 216, 54, 135, 233, 92, 224, 245, 245, 230, 225, 140, 242, 148, 39, 158, 237, 76, 240, 8, 112, 97, 181, 32, 134, 45, 48, 253, 31])
+
+                data = try! AES.GCM.open(
+                    try! AES.GCM.SealedBox(
+                        combined: data
+                    ),
+                    using: SymmetricKey(
+                        data: [254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254, 254]
+                    )
+                )
+
+                return String(
+                    bytes: data,
+                    encoding: .utf8
+                )!
+            }()
+            """,
+            macros: macros
+        )
+
+        ObfuscatedString.randomNumberGenerator = TestRandomNumberGenerator(seed: UInt64.max / 4 * 3)
+        assertMacroExpansion(
+            originalSource,
+            expandedSource: """
+            let string = {
+                var data = Data([253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 52, 126, 79, 230, 217, 73, 41, 70, 155, 200, 205, 161, 254, 173, 137, 51, 107, 26, 16, 41, 40, 51, 165, 75, 114, 184, 77, 30, 7, 241, 119, 239, 165, 111, 218, 186, 226, 131, 106, 112, 244, 45, 49, 131])
+
+                data = try! ChaChaPoly.open(
+                    try! ChaChaPoly.SealedBox(
+                        combined: data
+                    ),
+                    using: SymmetricKey(
+                        data: [253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253, 253]
+                    )
+                )
 
                 return String(
                     bytes: data,
