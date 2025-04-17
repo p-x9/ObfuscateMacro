3fe24134a13966b2017d3f4c1cbe34676166fbf6
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-04-17 19:50:01 +0900

    Support swift-syntax version <602.0.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
-            "509.0.0"..<"601.0.0-prerelease"
+            "509.0.0"..<"602.0.0"
         ),
         .package(
             url: "https://github.com/apple/swift-algorithms",
