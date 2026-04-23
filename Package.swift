7b1ce9a6e32e9b0d18bcc438468c3ede0f4b6d41
Greg Cotten <greg@gregcotten.com>
2026-04-22 21:08:53 -0700

    allow up to swift-syntax 603.0.1

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
-            "509.0.0"..<"603.0.0"
+            "509.0.0"..."603.0.1"
         ),
         .package(
             url: "https://github.com/apple/swift-algorithms",
